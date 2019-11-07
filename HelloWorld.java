a43c5dfeec597dd2099328d48bdf0d0cd7b82377
rmanushi <rmanushi@citycollege.sheffield.eu>
2019-11-07 14:12:04 +0200

    HelloWorld programm uploaded.

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Hello World!");
+		//System.out.println("Java is the best!");
+		//System.out.println("I am a computer.");
+		System.out.println("Hello World!\nJava is the best!\nI am a computer.");
+		
+
+	
+
+
+
+
+	}
+ 
+} // end of class 
